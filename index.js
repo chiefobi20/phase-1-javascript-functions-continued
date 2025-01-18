f016e77d5fa5843cfb1afba0b81ab22419cd39c8
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-01-18 18:29:33 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+
+
+function wrapAdjective(wrapped){
+    return function(innerFn){
+        return `You are ${wrapped}${innerFn}${wrapped}!`
+    }
+}
+console.log(wrapAdjective('||'))
